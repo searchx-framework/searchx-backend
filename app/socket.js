ff6c607e3d4a61c8c0f922fd99040f864cbd7fe7
sindunuragarp <sindunuragarp@gmail.com>
2018-01-02 22:31:34 +0100

    Added waiting & timeout for group topic sync

--- app/socket.js
+++ app/socket.js
@@ -8,28 +8,41 @@ module.exports = function(io) {
     gio.on('connection', (socket) => {
 
         socket.on('register', (data) => {
-            socket.userId = data.userId;
-            socket.groupId = Task.getGroupId(socket.userId);
-            socket.join(socket.groupId);
+            const groupId = Task.getGroupId(data.userId);
+
+            socket.groupId = groupId;
+            socket.join(groupId);
         });
 
-        socket.on('pretestScore', async (data) => {
-            await Task.savePretestScores(socket.userId, data.scores);
-            await Task.getGroupTopic(socket.groupId).then((topicId) => {
-                console.log(topicId);
-                if (topicId) {
-                    gio.to(socket.groupId).emit('groupTopic', {
-                        groupId: socket.groupId,
-                        topicId: topicId
-                    });
-                }
-            });
+        ////
+
+        socket.on('pushPretestScores', async (data) => {
+            await Task.savePretestScores(data.userId, data.scores);
+            const topicId = await Task.getGroupTopic(socket.groupId);
+
+            if (topicId) {
+                gio.to(socket.groupId).emit('groupTopic', {
+                    groupId: socket.groupId,
+                    topicId: topicId
+                });
+            }
+        });
+
+        socket.on('pushGroupTimeout', async () => {
+            const topicId = await Task.disableGroup(socket.groupId);
+
+            if (topicId) {
+                gio.to(socket.groupId).emit('groupTopic', {
+                    groupId: socket.groupId,
+                    topicId: topicId
+                });
+            }
         });
 
         ////
 
         socket.on('pushSearchState', (data) => {
-            socket.broadcast.to(socket.groupId).volatile.emit('syncSearch', data);
+            socket.broadcast.to(socket.groupId).volatile.emit('searchState', data);
         });
     });
 };

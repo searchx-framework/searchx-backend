3a9e46089ca067ef9609b919e0f1868bc2da830a
sindunuragarp <sindunuragarp@gmail.com>
2018-01-15 13:43:47 +0100

    Changed group creation to depend on pretest score matching (not on user code) - changed topic sampling to take into account past information (increase likelihood of topic match)

--- app/models/group.js
+++ app/models/group.js
@@ -4,32 +4,17 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const GroupSchema = new Schema({
-    groupId: {
-        type: String,
-        required: true
-    },
     created: {
         type: Date,
-        required: true
-    },
-    topics: {
-        type: Schema.Types.Mixed,
         required: true
     },
     members: {
         type: Schema.Types.Mixed,
         required: true
     },
-
-    scores: {
+    topic: {
         type: Schema.Types.Mixed,
-        default: {}
-    },
-    sessionId: {
-        type: String
-    },
-    assignedTopicId: {
-        type: String
+        required: true
     }
 }, {minimize: false});
 

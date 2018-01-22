77e5ed80830cd5016b5111bf62003deb5bb8b35d
sindunuragarp <sindunuragarp@gmail.com>
2018-01-22 20:46:15 +0100

    corrected bing api key

--- app/config/env/development.js
+++ app/config/env/development.js
@@ -6,5 +6,5 @@ module.exports = {
     client: 'http://localhost:3000',
     db: 'mongodb://localhost/searchx',
     redis: 'redis://localhost:6379',
-    bingAccessKey: '2dc977d4fe444173be6155559264e442', /* f.moraes */
+    bingAccessKey: '2dc977d4fe444173be6155559264e442', /* fe.moraesg@outlook.com */
 };

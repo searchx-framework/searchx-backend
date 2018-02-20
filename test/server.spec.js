43d511c212394a046792c25cdd21e2042af6bbd5
sindunuragarp <sindunuragarp@gmail.com>
2018-02-20 12:22:26 +0100

    Made clear that server test needs server to be running

---
+++ test/server.spec.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const supertest  = require('supertest');
+const should     = require('should');
+const shouldHttp = require('should-http');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const config = require('../app/config/config');
+const request = supertest(config.url + ':' + config.port + '/v1');
+
+describe('server (requires server to be running)', function() {
+    it('should be live', function(done) {
+        request
+            .get('/')
+            .end(function(err, res) {
+                if (err) {
+                    throw err;
+                }
+
+                res.should.have.status(418);
+                done();
+            });
+    });
+});

9e8f2b95397801d61ab338b7290877efd9065fcd
Kilian Grashoff <kiliangrashoff@gmail.com>
2018-03-21 02:32:23 +0100

    Set result count for images and videos correctly in regulator

--- app/services/search/regulator.js
+++ app/services/search/regulator.js
@@ -14,25 +14,26 @@ const bookmark = require('../../services/features/bookmark');
  * @params {providerName} the name of the search provider to use (bing by default)
  */
 exports.fetch = async function (query, vertical, pageNumber, sessionId, userId, providerName) {
+    const count = (vertical === 'images' || vertical === 'videos') ? 12 : 10;
     const bookmarks = await bookmark.getBookmarks(sessionId);
     const bookmarkedUrls = bookmarks.map(bookmark => bookmark.url);
+
     let accumulatedResults = [];
     let response;
 
     // for loop to limit maximum number of repeated queries
     for (let i = 0; i < 10; i++) {
-        console.log(i);
         response = await provider.fetch(query, vertical, pageNumber + i, providerName);
         let filteredResults = response.results;
         filteredResults = filteredResults.filter(result => {
             return !bookmarkedUrls.includes(result.url)
         });
         accumulatedResults = accumulatedResults.concat(filteredResults);
-        if (accumulatedResults.length >= 10) {
+        if (accumulatedResults.length >= count) {
             break
         }
     }
-    accumulatedResults = accumulatedResults.slice(0, 10);
+    accumulatedResults = accumulatedResults.slice(0, count);
     response.results = accumulatedResults;
     return response
 };

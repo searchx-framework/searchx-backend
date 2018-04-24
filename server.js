137d205e5e8188f89c47931e110a32b5225bafa8
Kilian Grashoff <kiliangrashoff@gmail.com>
2018-04-24 13:19:36 +0200

    Load newrelic in server

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 // Default to development environment if not set
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
+if (process.env.NODE_ENV === 'production') {
+    require('newrelic');
+}
+
 // Load the config
 const config = require('./app/config/config');
 

ffdcc19519e057d77844c22b7aadd14a0647200c
Kilian Grashoff <kiliangrashoff@gmail.com>
2018-07-26 11:33:26 +0200

    Only run bing tests if access key is set

--- app/config/config.js
+++ app/config/config.js
@@ -6,6 +6,6 @@ module.exports = {
     testUrl: 'http://localhost',
     cacheFreshness: 3600,
     scrapFreshness: 60 * 60 * 24,
-    bingAccessKey: '2dc977d4fe444173be6155559264e442', /* fe.moraesg@outlook.com */
+    bingAccessKey: undefined, /* fe.moraesg@outlook.com */
     defaultProvider: 'bing',
 };

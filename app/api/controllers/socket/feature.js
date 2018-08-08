f6dbecd96cd69f7b365f7ac1a1e79cf93a344b0d
Kilian Grashoff <kiliangrashoff@gmail.com>
2018-08-09 01:01:07 +0200

    Make realtime SERP updates non-volatile
    Now that we actually use the data in the update instead of refreshing
    the serp, missed messages will cause an inconsistent SERP state
    between users. Therefore messages need to be non-volatile.

--- app/api/controllers/socket/feature.js
+++ app/api/controllers/socket/feature.js
@@ -4,21 +4,21 @@ const queryhistory = require('../../../services/features/queryhistory');
 const view = require('../../../services/features/view');
 
 exports.broadcastSearchState = async function(socket, io, data) {
-    socket.broadcast.to(data.groupId).volatile.emit('searchState', data);
+    socket.broadcast.to(data.groupId).emit('searchState', data);
     queryhistory.pushQueryHistory(data.sessionId, data.userId, data.state.query)
         .catch((err) => console.log(err));
 };
 
 exports.broadcastViewState = async function(socket, io, data) {
-    socket.broadcast.to(data.groupId).volatile.emit('viewState', data);
+    socket.broadcast.to(data.groupId).emit('viewState', data);
     view.pushView(data.sessionId, data.userId, data.state.url)
         .catch((err) => console.log(err));
 };
 
 exports.broadcastBookmarkUpdate = async function(socket, io, data) {
-    socket.broadcast.to(data.groupId).volatile.emit('bookmarkUpdate', data.searchState);
+    socket.broadcast.to(data.groupId).emit('bookmarkUpdate', data.searchState);
 };
 
 exports.broadcastPageMetadataUpdate = async function(socket, io, data) {
-    socket.broadcast.to(data.groupId).volatile.emit('pageMetadataUpdate', data);
+    socket.broadcast.to(data.groupId).emit('pageMetadataUpdate', data);
 };
